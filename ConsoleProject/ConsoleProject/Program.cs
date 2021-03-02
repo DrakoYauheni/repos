a713f38c3c74c77893b374ba6e513f2ac3531b4b
Yauheni_Drako <lychponosa@gmail.com>
2021-03-02 19:01:17 +0300

    Создание тестового проекта

---
+++ ConsoleProject/ConsoleProject/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleProject
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            
+            Action<int,int> op;
+            op = Multiply;
+            Operation(20, 2, op);
+            op = Delenie;
+            Operation(50, 2, op);
+
+            Console.ReadLine();
+        }       
+
+        static void Operation(int x, int y, Action<int, int> op)
+        {
+            op(x, y);
+        }
+        static void Multiply(int x, int y)
+        {
+            Console.WriteLine(x * y);
+        }
+        static void Delenie(int x, int y)
+        {
+            Console.WriteLine(x / y);
+        }
+    }
+}
